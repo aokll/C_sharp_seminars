e33b9470159f5c5884cf3c2f5b3a29610f010f24
aokll <pastuhov_1992@mail.ru>
2023-02-27 10:34:13 +0300

    task2.2_finished

---
+++ Lesson2/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int TakeNum(int num)
+{
+    int res1 = num % 10;
+    int res2 = num / 10;
+
+    if(res1>res2){return res1}
+    else return res2;
+}
+int result = TakeNum(new Random().Next(10,100));
+Console.WriteLine(result);
