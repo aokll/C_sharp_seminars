4b83175b9cf61e5b6490bf639b2b5ee6803bd863
aokll <pastuhov_1992@mail.ru>
2023-02-27 11:23:08 +0300

    changed_task2.3

--- Lesson2/task3/Program.cs
+++ Lesson2/task3/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void exploer (int i1,int i2)
+{
+    if(i1 % i2 == 0)Console.WriteLine("кратно");
+    else Console.WriteLine($"не кратно{i1%i2}");
+}
+exploer(16,4);
