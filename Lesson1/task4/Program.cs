176d32c92e8f5d77b166e0d0ef14987c367d2fc4
aokll <pastuhov_1992@mail.ru>
2023-02-27 09:49:26 +0300

    task4_finished

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s_a = Console.ReadLine()!;
+	int a = int.Parse(s_a);
+	int b=-a;
+	
+	while(b<=a){
+	    Console.Write(b + " ");
+	    b++;
+	}
+
