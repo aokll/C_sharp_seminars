309408a7f197fcb45d7b53adea3ab811fe54e83a
aokll <pastuhov_1992@mail.ru>
2023-02-27 10:35:03 +0300

    task2.2_finished

--- Lesson2/task2/Program.cs
+++ Lesson2/task2/Program.cs
@@ -5,7 +5,7 @@ int TakeNum(int num)
     int res1 = num % 10;
     int res2 = num / 10;
 
-    if(res1>res2){return res1}
+    if(res1>res2){return res1;}
     else return res2;
 }
 int result = TakeNum(new Random().Next(10,100));
