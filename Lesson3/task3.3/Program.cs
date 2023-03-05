19e184059c3c171af25a714030178f9c05f5ba80
aokll <pastuhov_1992@mail.ru>
2023-03-05 12:02:17 +0300

    finished_task3.3

---
+++ Lesson3/task3.3/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double example(double x1,double x2,double y1,double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+}
+Console.WriteLine(example(3,6,2,1));
