2aea013cd9e21ee84b4e48d5a4aa0f60c852ce9d
aokll <pastuhov_1992@mail.ru>
2023-03-05 11:39:04 +0300

    finished_task3.2

--- Lesson3/task3.1/Program.cs
+++ Lesson3/task3.1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 void Quarter (int x,int y)
 {
-    if(x==0 || y==0);
+    if(x==0 || y==0)
         console.WriteLine("x=0, y=0");
-        else if(x>0 && y>0);
-        console.WriteLine("I");
-        else if(x<0 && y>0);
-        console.WriteLine("II");
-        else if(x<0 && y<0);
-        console.WriteLine("III");
-        else if(x>0 && y<0);
-        console.WriteLine("IV");
+        else if(x>0 && y>0)
+        Console.WriteLine("I");
+        else if(x<0 && y>0)
+        Console.WriteLine("II");
+        else if(x<0 && y<0)
+        Console.WriteLine("III");
+        else if(x>0 && y<0)
+        Console.WriteLine("IV");
 }
 int  num_1 = int.Parse(Console.ReadLine()!);
 int  num_2 = int.Parse(Console.ReadLine()!);
---
+++ Lesson3/task3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Quarter (int x)
+{
+        if(x==1)
+            Console.WriteLine("x>0, y>0");
+        else if(x==2)
+            Console.WriteLine("x<0, y>0");
+        else if(x==3)
+            Console.WriteLine("x<0 y<0");
+        else if(x==4)
+            Console.WriteLine("x>0 y<0");
+        else Console.WriteLine("error");
+}
+int  num_1 = int.Parse(Console.ReadLine()!);
+Quarter(num_1);
