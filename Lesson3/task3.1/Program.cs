695ce0c98a1654136a28f5752376b9226f61e32a
aokll <pastuhov_1992@mail.ru>
2023-03-05 11:13:09 +0300

    finished_task3.1

---
+++ Lesson3/task3.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Quarter (int x,int y)
+{
+    if(x==0 || y==0);
+        console.WriteLine("x=0, y=0");
+        else if(x>0 && y>0);
+        console.WriteLine("I");
+        else if(x<0 && y>0);
+        console.WriteLine("II");
+        else if(x<0 && y<0);
+        console.WriteLine("III");
+        else if(x>0 && y<0);
+        console.WriteLine("IV");
+}
+int  num_1 = int.Parse(Console.ReadLine()!);
+int  num_2 = int.Parse(Console.ReadLine()!);
+Quarter(num_1,num_2);
