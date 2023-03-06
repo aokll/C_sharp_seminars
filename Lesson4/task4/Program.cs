5444074eae7ceef4637314f8bea7bea7d6ad52c6
aokll <pastuhov_1992@mail.ru>
2023-03-06 11:49:48 +0300

    finished_task4,4

--- Lesson4/task4/Program.cs
+++ Lesson4/task4/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void Mass(int len)
+{
+    int[] r = new int[len];
+    for(int i = 0; i<len; i++){
+        r[i] = new Random().Next(2);
+        Console.WriteLine();
+        Console.WriteLine(r[i]);
+    }
+}
+Mass(int.Parse(Console.ReadLine()!));
