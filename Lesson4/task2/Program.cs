aaaa5cbb03f9834906893861b5fadd211a8eab61
aokll <pastuhov_1992@mail.ru>
2023-03-06 11:40:06 +0300

    finished_task4,2

--- Lesson4/task2/Program.cs
+++ Lesson4/task2/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int SumNum(int num)
+{
+    int k = 0;
+    for(int i = 1; num > 0; i++){
+        num = num/10;
+        k=i;
+    }
+    return k;
+}
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNum(a));
