e0006c8ab3ae8d4ed1ab8c267112e7ca4fa27673
aokll <pastuhov_1992@mail.ru>
2023-03-06 11:34:28 +0300

    finished_task4,1

---
+++ Lesson4/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int SumNum(int num)
+{
+    int all_sum = 0;
+    for(int i = 0; i<=num; i++) 
+    all_sum += i;
+    return all_sum;
+}
+int a = int.Parse(Console.ReadLine()!);
+int res = SumNum(a);
+Console.WriteLine(res);
