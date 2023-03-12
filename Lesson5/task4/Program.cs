6a81206554eb3d914e526ef45f165bf1ae3cafb9
aokll <pastuhov_1992@mail.ru>
2023-03-12 15:06:13 +0300

    finished_task5.4

--- Lesson5/task4/Program.cs
+++ Lesson5/task4/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void Print(int[] arr)
+	{
+	    int size = arr.Length;
+	
+	    for (int i = 0; i < size; i++)
+	        Console.Write($"{arr[i]} ");
+	    Console.WriteLine();
+	}
+	
+	int[] MassNums(int size, int from, int to)
+	{
+	    int[] arr = new int[size];
+	
+	    for (int i = 0; i < size; i++)
+	        arr[i] = new Random().Next(from, to);
+	    return arr;
+	}
+	
+	int SumPosNeg(int[] arr)
+	{
+	    int num_2 = 0;
+	
+	    for (int i = 0; i < arr.Length; i++)
+	    {
+	        if (10 <= arr[i] && arr[i] <= 99)
+	            num_2++;
+	
+	    }
+	    return num_2;
+	}
+	
+	
+	int num = int.Parse(Console.ReadLine()!);
+	int start = int.Parse(Console.ReadLine()!);
+	int stop = int.Parse(Console.ReadLine()!);
+	int[] mass = MassNums(num, start, stop);
+	Print(mass);
+	Console.WriteLine (SumPosNeg(mass));
